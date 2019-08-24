9f11de0799a57d043368d2852c294ab61566087c
theXtroyer1221 <50244210+theXtroyer1221@users.noreply.github.com>
2019-08-24 14:20:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+print("hej på dig, jag kommer ge dig en matte test får se om du svara rätt")
+
+import random
+
+num1 = random.randint(1,10)
+num2 = random.randint(1,11)
+answer = ""
+
+print("okej, vad är")
+
+print(num1, "+", num2)
+
+input(answer)
+
+if (answer is num1 + num2):
+
+ print("rätt! bra jobbat")
+
+else:
+
+ print("fel svar!")
+
+ #went badly wow
+
+ #dontdothisathome
+
+
+
+
+
